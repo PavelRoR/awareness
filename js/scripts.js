d9eed1258e4536ee4db9e3508b861d64900f5882
pavelp21 <pavelvp2182@yandex.ru>
2018-03-16 21:34:16 +0500

    Mail section is done

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('button.button').click(function (e) {
+        e.preventDefault;
+        alert('Еще не готово!');
+        return false;
+    })
+
+    /*Конец документа*/
+});
